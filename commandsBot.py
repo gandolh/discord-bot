29c8ae9305e52b9b97f348af2008adf63e4a8e45
gandolh <gusatualex@yahoo.com>
2021-11-09 11:52:42 +0200

    added help and embed into commandsBot

--- commandsBot.py
+++ commandsBot.py
@@ -1,10 +1,34 @@
 from discord.ext import commands
+import discord
 
 from dotenv import load_dotenv
 import os
 load_dotenv()
 botToken=os.getenv("botToken")
 bot =commands.Bot(command_prefix='!')
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed= discord.Embed(
+        title='TOP SECRET',
+        description='This is a private command.',
+        color=discord.Colour.green()
+    )
+    embed.set_thumbnail(url='https://i.pinimg.com/originals/bc/75/06/bc7506ba497f3aad78f8c0381c971cb6.jpg')
+    embed.add_field(
+        name='!help',
+        value='hmm',
+        inline=True
+    )   
+    embed.add_field(
+        name='!combo',
+        value='second hmm',
+        inline=True
+    )
+    await ctx.send(embed=embed)
+
 
 @bot.command() 
 async def info(ctx):
@@ -21,7 +45,7 @@ async def info(ctx):
 @bot.command()
 async def punch(ctx,arg):
     '''
-    !punch Justin
+    ex: !punch Justin
     '''
     await ctx.send(f'{str(ctx.author)[0:-5]} punched {arg}')
 
@@ -29,7 +53,7 @@ async def punch(ctx,arg):
 @bot.command()
 async def combo(ctx,*arg):
     '''
-    !punch Justin
+    combo people into one
     '''
     everyone= ' '.join(arg)
     await ctx.send(f'{str(ctx.author)[0:-5]} Comboed {everyone}')
