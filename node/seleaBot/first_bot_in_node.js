8087b337eb55cd8980722f69ca89a6bfa2ef3cbf
gandolh <gusatualex@yahoo.com>
2022-04-06 18:28:54 +0300

    try catch?

--- node/seleaBot/first_bot_in_node.js
+++ node/seleaBot/first_bot_in_node.js
@@ -11,13 +11,18 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
-	if (msg.channel.id == process.env.secretCommandsChanellId && msg.content[0] == "~" && msg.author.bot == false) {
-		channel = await client.channels.fetch(process.env.chanellToSend);
-		if (msg.content.slice(1, 13) == "random selea")
-			channel.send({ files: [{ attachment: `./imgs/${Math.floor(Math.random() * 11 + 1)}.jpg` }] });
-		else if (msg.content.slice(1, 5) == "send"){
-			channel.send({ files: [{ attachment: `./imgs/${msg.content.slice(6)}` }] });
+    try{
+        if (msg.channel.id == process.env.secretCommandsChanellId && msg.content[0] == "~" && msg.author.bot == false) {
+            channel = await client.channels.fetch(process.env.chanellToSend);
+            if (msg.content.slice(1, 13) == "random selea")
+                channel.send({ files: [{ attachment: `./imgs/${Math.floor(Math.random() * 11 + 1)}.jpg` }] });
+            else if (msg.content.slice(1, 5) == "send"){
+                channel.send({ files: [{ attachment: `./imgs/${msg.content.slice(6)}` }] });
+            }
+            else channel.send(msg.content.slice(1));
         }
-        else channel.send(msg.content.slice(1));
-	}
+    }catch(ex){
+        console.log(ex)
+    }
+
 });
