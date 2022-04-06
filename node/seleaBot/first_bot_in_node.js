5663780a0b3fc985d28c4f949f4a1b303ba6b575
gandolh <gusatualex@yahoo.com>
2022-04-06 18:02:23 +0300

    update database

--- node/seleaBot/first_bot_in_node.js
+++ node/seleaBot/first_bot_in_node.js
@@ -1,20 +1,22 @@
-console.log('beep beep')
+console.log("beep beep");
 
-const Discord    = require('discord.js');
-require('dotenv').config();
+const Discord = require("discord.js");
+require("dotenv").config();
 client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.login(process.env.botToken);
 
-client.on('ready',()=>{
-    console.log('ready')
-})
+client.on("ready", () => {
+	console.log("ready");
+});
 
-client.on('message',async(msg)=>{
-    if(msg.channel.id==process.env.secretCommandsChanellId && msg.content[0]=='~' && msg.author.bot==false) {
-        channel= await client.channels.fetch(process.env.chanellToSend)
-        if(msg.content.slice(1,13)=="random selea")
-        channel.send({ files: [{ attachment: `./imgs/${Math.floor(Math.random()*11 + 1)}.jpg` }] });
-        else channel.send(msg.content.slice(1))
-    }
-})
+client.on("message", async (msg) => {
+	if (msg.channel.id == process.env.secretCommandsChanellId && msg.content[0] == "~" && msg.author.bot == false) {
+		channel = await client.channels.fetch(process.env.chanellToSend);
+		if (msg.content.slice(1, 13) == "random selea")
+			channel.send({ files: [{ attachment: `./imgs/${Math.floor(Math.random() * 11 + 1)}.jpg` }] });
+		else if (msg.content.slice(1, 5) == "send")
+			channel.send({ files: [{ attachment: `./imgs/${msg.content.slice(6)}.jpg` }] });
+		else channel.send(msg.content.slice(1));
+	}
+});
